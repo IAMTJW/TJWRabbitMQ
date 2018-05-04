33c6e620c494ab63317cba5d529e5fea3ebd9281
tianjunwei <924862077@qq.com>
2018-05-04 22:52:15 +0800

    添加ack和持久化

--- src/main/java/com/tianjunwei/learn/queue/Receiver.java
+++ src/main/java/com/tianjunwei/learn/queue/Receiver.java
@@ -20,23 +20,47 @@ public class Receiver {
 	      throws java.io.IOException,
 	             java.lang.InterruptedException, TimeoutException {
 
+		//创建连接工厂
 		ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("localhost");
-	    Connection connection = factory.newConnection();
-	    Channel channel = connection.createChannel();
+		//IP地址
+		factory.setHost("127.0.0.1");
+		//端口
+		factory.setPort(5672);
+		//用户名
+		//factory.setUsername("guest");
+		//密码
+		//factory.setPassword("geust");
+		//创建连接
+		Connection connection = factory.newConnection();
+		//创建通道
+		Channel channel = connection.createChannel();
 
-	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		//设置持久化为true
+		boolean durable = true;
+		//指定要监听的队列名称
+	    channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
 	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+	    
+	    //创建消费监听者
 	    Consumer consumer = new DefaultConsumer(channel) {
 	    	  @Override
 	    	  public void handleDelivery(String consumerTag, Envelope envelope,
 	    	                             AMQP.BasicProperties properties, byte[] body)
 	    	      throws IOException {
-	    	    String message = new String(body, "UTF-8");
-	    	    System.out.println(" [x] Received '" + message + "'");
+	    		  try{
+	    			  String message = new String(body, "UTF-8");
+	    			  System.out.println(" [x] Received '" + message + "'");
+	    		  }finally {
+	    			  //收到消息确认
+	    			  channel.basicAck(envelope.getDeliveryTag(), false);
+				}
+	    	   
 	    	  }
 	    	};
-	    channel.basicConsume(QUEUE_NAME, true, consumer);
+	    //关闭消息自动确认机制，改为主动确认
+	    boolean autoAck = false;
+	    //启动消费监听者
+	    channel.basicConsume(QUEUE_NAME, autoAck, consumer);
 	}
 	
 }
--- src/main/java/com/tianjunwei/learn/queue/Send.java
+++ src/main/java/com/tianjunwei/learn/queue/Send.java
@@ -6,20 +6,35 @@ import java.util.concurrent.TimeoutException;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class Send {
 	
 	private final static String QUEUE_NAME = "hello1";
 
 	public static void main(String[] args) throws IOException, TimeoutException {
+		//创建连接工厂
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		//IP地址
+		factory.setHost("127.0.0.1");
+		//端口
+		factory.setPort(5672);
+		//用户名
+		//factory.setUsername("guest");
+		//密码
+		//factory.setPassword("geust");
+		//创建连接
 		Connection connection = factory.newConnection();
+		//创建通道
 		Channel channel = connection.createChannel();
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		//设置持久化为true
+		boolean durable = true;
+		//设置queue名称
+		channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
 		int i =0;
 		String message = "Hello World!"+i;
-		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		//向queue中发送数据
+		channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
 		System.out.println(" [x] Sent '" + message + "'");
 		i++;
 		channel.close();
